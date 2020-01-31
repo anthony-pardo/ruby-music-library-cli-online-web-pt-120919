5ce3827de526ba32bedb0aa91b36dbeb5ba11dad
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-31 19:59:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -88,7 +88,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     list_songs
-    input = gets.chomp
+    user_input = gets.chomp
     songs_list = Song.all.sort_by {|song| song.name}
     
     binding.pry
