cf2947a57740d61816a35a6debde25b53006e1c1
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-31 20:21:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -89,7 +89,7 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     #list_songs
     songs_list = Song.all.uniq.sort_by {|song| song.name}
-    input = gets.to_i
+    input = gets.chomp.to_i
     if input > 0 and input <= songs_list.size
       song = songs_list[input + 1]
       puts "Playing #{song.name} by #{song.artist.name}"
